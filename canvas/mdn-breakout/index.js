fe26f5b2b01967b9aa05f52eb22f2d9070fb6e5c
Kevin Labtani <kevinlabtani@gmail.com>
2019-10-29 10:58:44 +0100

    part 2 for canvas tutorial

--- canvas/mdn-breakout/index.js
+++ canvas/mdn-breakout/index.js
@@ -3,23 +3,28 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-// paint a red square
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+// coord
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+// change coord val to make it move
+let dx = 2;
+let dy = -2;
 
-//create a green circle
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+let drawBall = () => {
+  // paint the ball
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+};
 
-// paint a blue-stroked empty circle
-ctx.beginPath();
-ctx.arc(300, 100, 20, 0, Math.PI * 2, false);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+let draw = () => {
+  // clear canvas before each frame
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall()
+  x += dx;
+  y += dy;
+};
+
+setInterval(draw, 10);
