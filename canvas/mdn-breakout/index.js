e3dd8ecc78b8a23704d7bcf2ea8302b82cf034f7
Kevin Labtani <kevinlabtani@gmail.com>
2019-10-29 14:40:28 +0100

    part 6 for canvas tutorial

--- canvas/mdn-breakout/index.js
+++ canvas/mdn-breakout/index.js
@@ -24,8 +24,27 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
 
+// brick variables
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+// offset so they aren't drawn directly from the top left corner
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+// brick array
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
+
+// paint the ball
 const drawBall = () => {
-  // paint the ball
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
@@ -33,6 +52,7 @@ const drawBall = () => {
   ctx.closePath();
 };
 
+// paint the paddle
 const drawPaddle = () => {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -41,9 +61,27 @@ const drawPaddle = () => {
   ctx.closePath();
 };
 
+// paint the bricks
+const drawBricks = () => {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+      let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+    }
+  }
+};
+
 const draw = () => {
   // clear canvas before each frame
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
   drawBall();
   drawPaddle();
 
