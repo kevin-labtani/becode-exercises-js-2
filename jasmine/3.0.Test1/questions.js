dfd3305c44ccbb8d8b980bd1b0f2fdb4b92228a4
Kevin Labtani <kevinlabtani@gmail.com>
2019-10-29 10:07:19 +0100

    finish jasmine series 1

--- jasmine/3.0.Test1/questions.js
+++ jasmine/3.0.Test1/questions.js
@@ -3,64 +3,70 @@
  * Trouvez la façon de faire la plus optimal.
  * Il peut y avoir plusieur façon de faire.
  */
-let tailleString = (texte) => {
-    return texte.length;
-}
-let remplaceECar = (texte) => {
-    let a = texte.indexOf("e")
-    let text =  texte.substr(" ", a) + " " +texte.substr(a + 1 , texte.length);
-    return text;
-}
+let tailleString = texte => {
+  return texte.length;
+};
+let remplaceECar = texte => {
+  let a = texte.indexOf("e");
+  let text = texte.substr(" ", a) + " " + texte.substr(a + 1, texte.length);
+  return text;
+};
 let concatString = (texte1, texte2) => {
+  return texte1 + texte2;
+};
+let afficherCar5 = texte => {
+  return texte.charAt(4);
+};
+let afficher9Car = texte => {
+  return texte.substr(0, 9);
+};
+let majusculeString = texte => {
+  return texte.toUpperCase();
+};
+let minusculeString = texte => {
+  return texte.toLowerCase();
+};
+let SupprEspaceString = texte => {
+  return texte.trim();
+};
+let IsString = texte => {
+  return typeof texte === "string";
+};
 
-}
-let afficherCar5 =  (texte) => {
-
-}
-let afficher9Car =  (texte) => {
-
-}
-let majusculeString =  (texte) => {
-
-}
-let minusculeString =  (texte) => {
-
-}
-let SupprEspaceString =  (texte) => {
-
-}
-let IsString =  (texte) => {
-
-}
-
-let AfficherExtensionString =  (texte) => {
-
-}
-let NombreEspaceString =  (texte) => {
-
-}
-let InverseString =  (texte) => {
-
-}
+let AfficherExtensionString = texte => {
+  const arr = texte.split(".");
+  return arr[1];
+};
+let NombreEspaceString = texte => {
+  let count = texte.match(/[ ]/g);
+  return count.length;
+};
+let InverseString = texte => {
+  return texte
+    .split("")
+    .reverse()
+    .join("");
+};
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
-let calculPuissance =  (x, y) => {
-
-}
-let valeurAbsolue =  (nombre) => {
-
-}
-let valeurAbsolueArray =  (array) => {
-
-}
-let sufaceCercle =  (rayon) => {
-
-}
-let hypothenuse =  (ab, ac) => {
-
-}
-let calculIMC =  (poids, taille) => {
-
-}
+let calculPuissance = (x, y) => {
+  return Math.pow(x, y);
+};
+let valeurAbsolue = nombre => {
+  return Math.abs(nombre);
+};
+let valeurAbsolueArray = array => {
+  const newArray = array.map(num => Math.abs(num));
+  return newArray;
+};
+let sufaceCercle = rayon => {
+  return Math.round(Math.PI * Math.pow(rayon, 2));
+};
+let hypothenuse = (ab, ac) => {
+  return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
+};
+let calculIMC = (poids, taille) => {
+  return parseFloat((poids / Math.pow(taille, 2)).toFixed(2));
+};
